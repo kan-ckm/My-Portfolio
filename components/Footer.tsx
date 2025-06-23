3062d0bdc9cbc62a51dbd0d4ae240d03ee775dc6
Tuấn Anh <at2356116@gmail.com>
2025-06-23 17:01:19 +0700

    update code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                 </a>
             </div>
             <div className='flex  mt-16 md:flex-row flex-col justify-between items-center'>
-                <p className='md:text-base text-sm md:font-normal font-light'>Copy right © 2025 Tuan Anh</p>
+                <p className='md:text-base text-sm md:font-normal font-light'>Copy right © 2025 Kan-kcm</p>
                 <div className='flex items-center md:gap-3 gap-6'>
                     {
                         socialMedia.map((profile) => (
